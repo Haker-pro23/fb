7526a99600bc081de3c750c06c51e387baf1c695
Haker-pro23 <117530681+Haker-pro23@users.noreply.github.com>
2022-11-05 16:50:37 +0100

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,33 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+os.system ('termux-open-url https://api.whatsapp.com/send?phone=+212774958938')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;32m')
+    os.system ('clear')
+    print ("██╗░░██╗░█████╗░░█████╗░██╗░░██╗░░░░░███████╗██████╗░")
+    print ("██║░░██║██╔══██╗██╔══██╗██║░██╔╝░░░░░██╔════╝██╔══██╗ ")
+    print ("███████║███████║██║░░╚═╝█████═╝░░░░░░█████╗░░██████╦╝")
+    print ("██╔══██║██╔══██║██║░░██╗██╔═██╗░░░░░░██╔══╝░░██╔══██╗")
+    print( "██║░░██║██║░░██║╚█████╔╝██║░╚██╗░░░░░██║░░░░░██████╦╝")
+    print ("╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░░░░░╚═╝░░░░░╚═════╝░")
+    time.sleep(0.2)
+    print ('\033[1;31m  ﺔقدﺎﺼﻤﻟا زوﺎﭴﺗ')
+    fx9 = input (" \033[1;33m  ﺔﯾﺤﺿ بﺎﺴﺤ طﺑار  ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == 'https://www.facebook.com/profile.php?id=1000292983282' :
+            os.system ('python in.py')
+            restart ()
+    elif fx9 == 'Yasser YL' :
+            os.system ('python rot.py')
+main (main)
+home_main ()
+x
